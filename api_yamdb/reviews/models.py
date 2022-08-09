e84577f646678350227aca57a899556ea244d08b
Вадим Волковский <vadimas29@gmail.com>
2022-08-09 14:10:58 +0300

    убрал choices и внес мелкие правки

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -2,12 +2,7 @@ from django.db import models
 
 
 class Category(models.Model):
-    CATEGORY_CHOICES = (
-        ('books', 'Книги'),
-        ('films', 'Фильмы'),
-        ('music', 'Музыка'),
-    )
-    name = models.CharField(max_length=256, choices=CATEGORY_CHOICES)
+    name = models.CharField()
     slug = models.SlugField(unique=True, max_length=50)
 
     def __str__(self):
@@ -15,24 +10,7 @@ class Category(models.Model):
 
 
 class Genre(models.Model):
-    GENRE_CHOICES = (
-        ('drama', 'Драма'),
-        ('comedy', 'Комедия'),
-        ('western', 'Вестерн'),
-        ('fantasy', 'Фэнтези'),
-        ('sci-fi', 'Фантастика'),
-        ('detective', 'Детектив'),
-        ('thriller', 'Триллер'),
-        ('tale', 'Сказка'),
-        ('gonzo', 'Гонзо'),
-        ('roman', 'Роман'),
-        ('ballad', 'Баллада'),
-        ('rock-n-roll', 'Rock-n-roll'),
-        ('classical', 'Классика'),
-        ('rock', 'Рок'),
-        ('chanson', 'Шансон'),
-    )
-    name = models.CharField(max_length=256, choices=GENRE_CHOICES)
+    name = models.CharField()
     slug = models.SlugField(unique=True, max_length=50)
 
     def __str__(self):
@@ -42,8 +20,12 @@ class Genre(models.Model):
 class Titles(models.Model):
     name = models.CharField(max_length=100)
     year = models.IntegerField()
-    genre = models.ManyToManyField(Genre)
-    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
+    genre = models.ManyToManyField(Genre, related_name='genres')
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.SET_NULL,
+        null=True
+    )
     description = models.TextField(max_length=200, blank=True)
 
     def __str__(self):
