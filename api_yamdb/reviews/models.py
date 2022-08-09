89ed500c6dccef055cb412570e6c9905b026ae7f
Вадим Волковский <vadimas29@gmail.com>
2022-08-09 10:07:59 +0300

    added models 'Category', 'Genre', 'Titles'

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,3 +1,38 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    CATEGORY_CHOICE = ['Книги', 'Фильмы', 'Музыка']
+    name = models.CharField(choices=CATEGORY_CHOICE)
+    slug = models.SlugField(unique=True)
+
+
+class Genre(models.Model):
+    GENRE_CHOICE = [
+        'Драма',
+        'Комедия',
+        'Вестерн',
+        'Фэнтези',
+        'Фантастика',
+        'Детектив',
+        'Триллер',
+        'Сказка',
+        'Гонзо',
+        'Роман',
+        'Баллада',
+        'Rock-n-roll',
+        'Классика',
+        'Рок',
+        'Шансон'
+    ]
+    name = models.CharField(choices=GENRE_CHOICE)
+    slug = models.SlugField(unique=True)
+
+
+class Titles(models.Model):
+    name = models.CharField(max_length=100)
+    year = models.IntegerField(max_length=4)
+    genre = models.ManyToManyField(Genre, on_delete=models.SET_NULL)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL)
+    description = models.TextField(max_length=200)
+
