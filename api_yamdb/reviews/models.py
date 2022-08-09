b279b84010701070e97a683f1a954827b5858d15
Игорь Шкода <port_tf@bk.ru>
2022-08-09 14:44:00 +0300

    Пробный юзер

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth.models import AbstractUser
+
+
+CHOICES_ROLE = [
+    ('user', 'user'),
+    ('moderator', 'moderator'),
+    ('admin', 'admin')
+]
+
+class User(AbstractUser):
+    username = models.CharField('Ник пользователя', unique=True, max_length=150)
+    email = models.EmailField('e-mail пользователя')
+    first_name = models.CharField('Имя пользователя', max_length=150)
+    last_name = models.CharField('Фамилия пользователя', max_length=150)
+    bio = models.TextField('Биография', blank=True)
+    role = models.CharField('Роль пользователя', default='user', max_length=50, choices=CHOICES_ROLE)
