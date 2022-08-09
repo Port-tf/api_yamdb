59cc8d6cd0c3b4d22397eca43f0def31e8f4a2eb
Вадим Волковский <vadimas29@gmail.com>
2022-08-09 11:21:20 +0300

    V2. edited Category, Genre, Titles

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -2,37 +2,50 @@ from django.db import models
 
 
 class Category(models.Model):
-    CATEGORY_CHOICE = ['Книги', 'Фильмы', 'Музыка']
-    name = models.CharField(choices=CATEGORY_CHOICE)
-    slug = models.SlugField(unique=True)
+    CATEGORY_CHOICES = (
+        ('books', 'Книги'),
+        ('films', 'Фильмы'),
+        ('music', 'Музыка'),
+    )
+    name = models.CharField(max_length=256, choices=CATEGORY_CHOICES)
+    slug = models.SlugField(unique=True, max_length=50)
+
+    def __str__(self):
+        return self.name
 
 
 class Genre(models.Model):
-    GENRE_CHOICE = [
-        'Драма',
-        'Комедия',
-        'Вестерн',
-        'Фэнтези',
-        'Фантастика',
-        'Детектив',
-        'Триллер',
-        'Сказка',
-        'Гонзо',
-        'Роман',
-        'Баллада',
-        'Rock-n-roll',
-        'Классика',
-        'Рок',
-        'Шансон'
-    ]
-    name = models.CharField(choices=GENRE_CHOICE)
-    slug = models.SlugField(unique=True)
+    GENRE_CHOICES = (
+        ('drama', 'Драма'),
+        ('comedy', 'Комедия'),
+        ('western', 'Вестерн'),
+        ('fantasy', 'Фэнтези'),
+        ('sci-fi', 'Фантастика'),
+        ('detective', 'Детектив'),
+        ('thriller', 'Триллер'),
+        ('tale', 'Сказка'),
+        ('gonzo', 'Гонзо'),
+        ('roman', 'Роман'),
+        ('ballad', 'Баллада'),
+        ('rock-n-roll', 'Rock-n-roll'),
+        ('classical', 'Классика'),
+        ('rock', 'Рок'),
+        ('chanson', 'Шансон'),
+    )
+    name = models.CharField(max_length=256, choices=GENRE_CHOICES)
+    slug = models.SlugField(unique=True, max_length=50)
+
+    def __str__(self):
+        return self.name
 
 
 class Titles(models.Model):
     name = models.CharField(max_length=100)
-    year = models.IntegerField(max_length=4)
-    genre = models.ManyToManyField(Genre, on_delete=models.SET_NULL)
-    category = models.ForeignKey(Category, on_delete=models.SET_NULL)
-    description = models.TextField(max_length=200)
+    year = models.IntegerField()
+    genre = models.ManyToManyField(Genre)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
+    description = models.TextField(max_length=200, blank=True)
+
+    def __str__(self):
+        return self.name
 
