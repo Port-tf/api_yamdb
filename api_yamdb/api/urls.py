6a0ecdf8bd77d01a01192886313bf001c3a38978
Вадим Волковский <vadimas29@gmail.com>
2022-08-10 13:57:25 +0300

    added endpoints: category, genre, titles

---
+++ api_yamdb/api/urls.py
@@ -0,0 +1,21 @@
+from django.urls import include, path
+from rest_framework.routers import DefaultRouter
+from api.views import *
+
+app_name = 'api'
+
+router = DefaultRouter()
+
+router.register(r'categories', CategoryViewSet)
+router.register(r'genres', GenreViewSet)
+router.register(r'titles', TitlesViewSet)
+router.register(r'titles/(?P<title_id>\d+)/reviews/(?P<review_id>\d+)/comments',
+                CommentViewSet, basename='comments')
+router.register(r'titles/(?P<title_id>\d+)/reviews',
+                ReviewViewSet, basename='reviews')
+
+urlpatterns = [
+    path('v1/', include(router.urls)),
+    path('v1/', include('djoser.urls')),
+    path('v1/', include('djoser.urls.jwt'))
+]
