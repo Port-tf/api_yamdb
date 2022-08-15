7d0e1a3698415554447f727dfb8070b4939979b6
Cortin34 <cortin@mail.ru>
2022-08-15 20:57:29 +0300

    Валидация me

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -11,6 +11,13 @@ class SignUpSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ('username', 'email')
+    
+    def validate_username(self, value):
+        if value == 'me':
+            raise serializers.ValidationError(
+                'Имя пользователя "me" не разрешено.'
+            )
+        return value
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -19,6 +26,25 @@ class UserSerializer(serializers.ModelSerializer):
         model = User
         fields = ('username', 'email', 'first_name',
                   'last_name', 'bio', 'role')
+    
+    # def validate(self, data):
+    #     print(self.context)
+    #     author = self.context.get('request').user
+    #     title_id = self.context.get('view').kwargs.get('title_id')
+    #     title = get_object_or_404(Title, id=title_id)
+    #     if (
+    #         self.context.get('request').method == 'POST'
+    #         and Review.objects.filter(title_id=title.id, author=author).exists()
+    #     ):
+    #         raise ValidationError('Может существовать только один отзыв!')
+    #     return data    
+
+    # def validate_username(self, value):
+    #     if value == 'me':
+    #         raise serializers.ValidationError(
+    #             'Имя пользователя "me" не разрешено.'
+    #         )
+    #     return value
 
 
 class CategorySerializer(serializers.ModelSerializer):
@@ -82,7 +108,7 @@ class TitlesSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Title
-        fields = ('id', 'name', 'year', 'genre', 
+        fields = ('id', 'name', 'year', 'genre',
                   'category', 'description', 'rating'
                   )
 
@@ -90,7 +116,9 @@ class TitlesSerializer(serializers.ModelSerializer):
         """Считает средний рейтинг произведения"""
         title_rating = obj.reviews.aggregate(rating=Avg('score'))
         rating = title_rating.get('rating')
-        return rating
+        if rating is None:
+            return None
+        return round(rating, 1)
 
 class ReviewSerializer(serializers.ModelSerializer):
     author = serializers.SlugRelatedField(
--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -33,8 +33,8 @@ class SignUpApiView(APIView):
             print(f'Посмотри, пришел на поклон: {serializer}')
             code = default_token_generator.make_token(user)
             send_mail('Subject here', code,'1@api.api', [request.data.get('email')],)
-            return Response(serializer.data, status=status.HTTP_201_CREATED) 
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST) 
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 
@@ -42,7 +42,7 @@ class SignUpApiView(APIView):
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
-    permission_classes = [IsAuthenticated, AdminPermission]
+    permission_classes = [AdminPermission, IsAuthenticated]
     lookup_field = 'username'
 
     @action(methods=['patch', 'get'], detail=True)
