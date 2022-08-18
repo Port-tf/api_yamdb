453c3f122285173d67c12dc4b5fd56204892aced
Игорь Шкода <port_tf@bk.ru>
2022-08-18 22:42:29 +0300

    Слияние от Игоря views

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,9 +1,8 @@
 from django.contrib.auth.tokens import default_token_generator
 from django.core.mail import send_mail
-from django.db.models import Avg
 from django.db import IntegrityError
+from django.db.models import Avg
 from django.shortcuts import get_object_or_404
-from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import filters, mixins, status, viewsets
 from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated
@@ -12,15 +11,16 @@ from rest_framework.views import APIView
 from rest_framework_simplejwt.tokens import RefreshToken
 
 from api.filters import TitleFilter
-from .permissions import (IsAdminOrReadOnly, IsAdmin,
-                          IsAuthorOrModeRatOrOrAdminOrReadOnly)
 from api.serializers import (CategorySerializer, CommentSerializer,
                              GenreSerializer, ReviewSerializer,
                              SignUpSerializer, TitlePostSerialzier,
                              TitleSerializer, TokenRegSerializer,
                              UserEditSerializer, UserSerializer)
+
 from api_yamdb.settings import DEFAULT_FROM_EMAIL
 from reviews.models import Category, Genre, Review, Title
+from .permissions import (IsAdmin, IsAdminOrReadOnly,
+                          IsAuthorOrModeRatOrOrAdminOrReadOnly)
 from users.models import User
 
 
